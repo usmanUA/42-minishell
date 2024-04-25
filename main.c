accfbe6de3a06300800c9d1274d981e2f01c470c
Ahmed <adusman@lm1-500-25097.local>
2024-04-25 09:58:18 +0300

    added memory freeing funcs

--- main.c
+++ main.c
@@ -11,11 +11,36 @@
 /* ************************************************************************** */
 #include "miniwell.h"
 #include "vec/vec.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-void	ft_free_vecs(void *vecs, int cmd)	
+void	ft_free_redirect(t_vec *redirect)
 {
+    t_redirect *fds;
+    int ind;
 
+    ind = -1;
+    while (++ind < redirect->len)
+    {
+	fds = (t_redirect *)&redirect->mem[ind*redirect->size];
+	close(fds->new_fd);
+	free(fds);
+    }
+    vec_free(redirect);
+}
+
+void	ft_free_cmd(t_vec *cmd)
+{
+    char *str;
+    int ind;
+
+    ind = -1;
+    while (++ind < cmd->len)
+    {
+	str = (char *)&cmd->mem[ind*cmd->len];
+	free(str);
+    }
+    vec_free(cmd);
 }
 
 void	ft_free_input(void *inpt)
@@ -23,26 +48,32 @@ void	ft_free_input(void *inpt)
     t_input *input;
 
     input = (t_input *)inpt;
-    ft_free_vecs(input, 0);
-    ft_free_vecs(input, 1);
+    ft_free_cmd(input->cmd);
+    ft_free_redirect(input->redirect);
     free(input);
 }
 void	ft_free_vec(t_vec *pipes)
 {
-   // TODO:
-    // LOOP over 
     int ind;
     t_input *input;
     
     ind = -1;
     while (++ind < pipes->len)
-	ft_free_input(pipes->mem[ind*pipes->size]);
+	ft_free_input(&pipes->mem[ind*pipes->size]);
 }
 
 int main(void)
 {
     t_vec   pipes;
     t_vars	vars;
+    int ind;
+    int i;
+    int j;
+    t_input *input;
+    t_vec cmd;
+    t_vec redirect;
+    int fd;
+    t_redirect fds;
 
     while (42)
     {
@@ -72,6 +103,23 @@ int main(void)
 	// 3. free input.cmd and input.redirect vectors
 	// NOTE: Of course there's a lot missing which I did not add to TODO list
 	// NOTE: FREE everything
+	ind = -1;
+	while (++ind < pipes.len)	
+	{
+	    input = (t_input *)&pipes.mem[ind*pipes.size];
+	    cmd = *input->cmd;
+	    redirect = *input->redirect;
+	    i = -1;
+	    while (++i < cmd.len)
+		printf("cmd and args: %s", (char *)&cmd.mem[i*cmd.size]);
+	    j = -1;
+	    while (++i < redirect.len)
+	    {
+		fds = ((t_redirect *)redirect.mem[i*redirect.size])->orig_fd;
+		fd = 
+		printf("orig fd: %d", fd);
+	    }
+	}
 	ft_free_vec(&pipes);	
     }
 }
